308437ff6f056b264a5f0e46521733f467cc4f8d
FujiharaAsuka <241070@std.hi-joho.ac.jp>
2025-06-11 09:24:10 +0900

    Chapter9

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
-// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
-        // 引数あり
-        Hero h1 = new Hero("ミナト");
-        System.out.println(h1.name);
         // 引数なし
-        Hero h2 = new Hero();
-        System.out.println(h2.name);
-    //w.heal(h1);
-    //w.heal(h2);
-    //w.heal(h2);
+        Hero h = new Hero();//同じ名前
+        System.out.println(h.name + "のHPは" + h.hp + "です");
+        // 引数あり String
+        Hero h1 = new Hero("ミナト");//同じ名前
+        System.out.println(h1.name + "のHPは" + h1.hp + "です");
+        // 引数あり int
+        Hero h2 = new Hero(999);//同じ名前
+        System.out.println(h2.name + "のHPは" + h2.hp + "です");
+        // 引数2つ
+        Hero h3 = new Hero(999, "ミナト");//同じ名前
+        System.out.println(h3.name + "のHPは" + h3.hp + "です");
     }
 }
